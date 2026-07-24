2ed058769a50bce04800b20e3bfa9fdebadbeaeb
suelenadles <suelenadles@hotmail.com>
2026-07-24 12:30:16 -0300

    feat(service): create user service

---
+++ backend/supportflow-api/src/main/java/com/supportflow/helpdesk/service/UserService.java
@@ -0,0 +1,35 @@
+package com.supportflow.helpdesk.service;
+
+import com.supportflow.helpdesk.domain.entity.User;
+import com.supportflow.helpdesk.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();}
+    
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+    
+}
