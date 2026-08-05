afc54315133bd127f88f5794cafe74b6cd0dbdb4
suelenadles <suelenadles@hotmail.com>
2026-08-05 20:01:08 -0300

    feat(security):add JWT authentication filter

--- backend/supportflow-api/src/main/java/com/supportflow/helpdesk/security/SecurityConfig.java
+++ backend/supportflow-api/src/main/java/com/supportflow/helpdesk/security/SecurityConfig.java
@@ -9,10 +9,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) 
@@ -29,6 +36,10 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
 
+            http.addFilterBefore(
+                jwtAuthenticationFilter, 
+                UsernamePasswordAuthenticationFilter.class
+            );
                  return http.build();
     }
 
